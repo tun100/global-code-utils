5e15050444b1c483b0545221f8bacb468cd3ccdc
lhm <laihuanminpc>
2019-07-07 10:04:03 +0800

    rebase code

---
+++ src/index.js
@@ -0,0 +1,55 @@
+var sh = require('shelljs')
+var _ = require('lodash')
+var moment = require('moment')
+var fs = require('fs')
+var path = require('path')
+var utils = {
+  loopFile (mypath, handlefunc) {
+    if (utils.isFile(mypath)) {
+      handlefunc(mypath)
+    } else {
+      var mychildren = utils.readDir(mypath)
+      _.forEach(mychildren, childName => {
+        var fullChildPath = path.join(mypath, childName)
+        utils.loopFile(fullChildPath, handlefunc)
+      })
+    }
+  },
+  readDir (mypath) {
+    return fs.readdirSync(mypath)
+  },
+  getCrtPath (relativePath) {
+    return path.join(__dirname, relativePath)
+  },
+  isSameValueOnPos (obj, key, value) {
+    return _.get(obj, key) === value
+  },
+  writeStringToFile (targetPath, ctn) {
+    if (!_.isString(ctn)) {
+      ctn = JSON.stringify(ctn)
+    }
+    return fs.writeFileSync(targetPath, ctn)
+  },
+  writeStringToRelativeFile (targetPath, ctn) {
+    return utils.writeStringToFile(utils.getCrtPath(targetPath), ctn)
+  },
+  readFileToString (targetPath) {
+    return fs.readFileSync(targetPath, 'UTF-8')
+  },
+  readRelativeFileToString (targetPath) {
+    return utils.readFileToString(utils.getCrtPath(targetPath))
+  },
+  log (str) {
+    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]: ${JSON.stringify(str)}`)
+  },
+  isDir (path) {
+    var info = fs.statSync(path)
+    return info.isDir()
+  },
+  isFile (path) {
+    var info = fs.statSync(path)
+    return info.isFile()
+  }
+}
+
+module.exports = utils
